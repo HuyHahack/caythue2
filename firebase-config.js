5149f84f00366bacfec1f662d7fc36c893c3c2d0
HuyHahack <huyg00046@gmail.com>
2025-02-22 09:46:14 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-dsHg-ltxjgoMvRdqmiLg0kicmeOFrds",
+    authDomain: "caythue-58418.firebaseapp.com",
+    databaseURL: "https://console.firebase.google.com/u/0/project/caythue-58418/database/caythue-58418-default-rtdb/data/~2F",
+    projectId: "caythue-58418",
+    storageBucket: "caythue-58418.firebasestorage.app",
+    messagingSenderId: "583803662779",
+    appId: "1:583803662779:web:ef8f8ff3fdbcee7299d49e"
+};
+
+// Khởi tạo Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
